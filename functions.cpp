a6cb9742930fb5ef14143254c0f4ce22e4483322
Ingenieria Sistemas31 <isi31@lab.uca.edu.ni>
2023-04-28 08:23:29 -0600

    Functions added

--- functions.cpp
+++ functions.cpp
@@ -1,3 +1,41 @@
-#include<iostream>
+#include <iostream>
+#include <cmath>
 
 using namespace std;
+
+bool primeNumber(int num) {
+    if (num <= 1) {
+        return false;
+    }
+
+    int raiz = sqrt(num);
+
+    for (int i = 2; i <= raiz; i++) {
+        if (num % i == 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+void addNums(int num, int& pares, int& impares, int& neutros) {
+    if (num == 0) {
+        neutros++;
+    } else if (num % 2 == 0) {
+        pares++;
+    } else {
+        impares++;
+    }
+}
+
+
+void multiple3(int num){
+    cout << "Los siguientes 10 numeros multiplos de 3 despues de " << num << " son:" << endl;
+    for (int i = num + 3; i <= num + 30; i += 3) {
+        cout << i << endl;
+    }
+}
+
+
+
